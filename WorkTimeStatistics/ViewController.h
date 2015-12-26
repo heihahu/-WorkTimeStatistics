a9dab00d95c5812d9f3563d051d9bb8cce8290c9
heihahu <dyzhao1989@hotmail.com>
2015-12-26 17:00:37 +0800

    Initial Commit

---
+++ WorkTimeStatistics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkTimeStatistics
+//
+//  Created by ZhaoJohn on 15/12/26.
+//  Copyright © 2015年 JohnZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WorkTimeStatistics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorkTimeStatistics
+//
+//  Created by ZhaoJohn on 15/12/26.
+//  Copyright © 2015年 JohnZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
